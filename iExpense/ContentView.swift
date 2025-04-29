6fa7f3fc142ae787a3f007a501e4e1228ac58753
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-29 23:15:14 +0500

    Saving Data

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -17,7 +17,16 @@ struct ContentView: View {
         {
             List{
                 ForEach(expenses.items){ item in
-                    Text(item.name)
+                    HStack
+                    {
+                        VStack(alignment: .leading)
+                        {
+                            Text(item.name).font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }.onDelete(perform: removeltems)
                 
             }.navigationTitle("iExpenses")
